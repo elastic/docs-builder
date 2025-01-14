86a2e4f167d5eb8a9c6b0286c81a90d4246e37ec
Martijn Laarman <Mpdreamz@gmail.com>
2025-01-14 20:00:51 +0100

    Add auto text link generation for markdown files
    Enhanced link handling to auto-generate text for markdown file links and headings when no link text is provided. Updated documentation with examples and fixed heading anchors to support new functionality.

--- src/Elastic.Markdown/Myst/InlineParsers/DiagnosticLinkInlineParser.cs
+++ src/Elastic.Markdown/Myst/InlineParsers/DiagnosticLinkInlineParser.cs
@@ -108,9 +108,20 @@ public class DiagnosticLinkInlineParser : LinkInlineParser
 
 		if (link.FirstChild == null || !string.IsNullOrEmpty(anchor))
 		{
-			var file = string.IsNullOrWhiteSpace(url) ? context.Path
-				: context.Build.ReadFileSystem.FileInfo.New(Path.Combine(context.Build.SourcePath.FullName, url.TrimStart('/')));
+			var file = string.IsNullOrWhiteSpace(url)
+				? context.Path
+				: url.StartsWith('/')
+					? context.Build.ReadFileSystem.FileInfo.New(Path.Combine(context.Build.SourcePath.FullName, url.TrimStart('/')))
+					: context.Build.ReadFileSystem.FileInfo.New(Path.Combine(context.Path.Directory!.FullName, url));
 			var markdown = context.GetDocumentationFile?.Invoke(file) as MarkdownFile;
+			if (markdown == null)
+			{
+				processor.EmitWarning(line,
+					column,
+					length,
+					$"Can not create an auto title link to '{file.FullName}' however this file can not be resolved.");
+			}
+
 			var title = markdown?.Title;
 
 			if (!string.IsNullOrEmpty(anchor))
