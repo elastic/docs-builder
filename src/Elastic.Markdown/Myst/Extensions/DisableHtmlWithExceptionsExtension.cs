9e89ea592e7564bd18ecb3e46dd3d1c56b44d06e
Jan Calanog <jan.calanog@elastic.co>
2025-01-24 13:54:38 +0100

    Add comment

--- src/Elastic.Markdown/Myst/Extensions/DisableHtmlWithExceptionsExtension.cs
+++ src/Elastic.Markdown/Myst/Extensions/DisableHtmlWithExceptionsExtension.cs
@@ -11,6 +11,10 @@ namespace Elastic.Markdown.Myst.Extensions;
 
 public static class MarkdownPipelineBuilderExtensions
 {
+	// This is a copy of the DisableHtml extension method with the addition of a HashSet of exceptions
+	// to allow certain HTML tags to be parsed.
+	// However, as of now it only supports inline HTML tags.
+	// If you need to support block HTML tags, you will need to add a custom HtmlBlockParser.
 	public static MarkdownPipelineBuilder DisableHtmlWithExceptions(this MarkdownPipelineBuilder pipeline, HashSet<string> exceptions)
 	{
 		var parser = pipeline.BlockParsers.Find<HtmlBlockParser>();
@@ -18,7 +22,6 @@ public static class MarkdownPipelineBuilderExtensions
 		{
 			pipeline.BlockParsers.Remove(parser);
 		}
-
 		pipeline.InlineParsers.ReplaceOrAdd<AutolinkInlineParser>(new RestrictedAutolinkInlineParser { AllowedTags = exceptions });
 		return pipeline;
 	}
