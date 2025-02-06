22dd5fdc7c36348862d90f717ed29c49478513d7
Martijn Laarman <Mpdreamz@gmail.com>
2025-02-06 12:37:37 +0100

    fix license header

--- tests/authoring/Blocks/Lists.fs
+++ tests/authoring/Blocks/Lists.fs
@@ -1,4 +1,7 @@
-module ``block elements``.``lists``
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+module ``block elements``.``list elements``
 
 open Xunit
 open authoring
