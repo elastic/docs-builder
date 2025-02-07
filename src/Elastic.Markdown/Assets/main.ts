89a84007553cbc4f6ce014abf9d2727fa90316bd
Jan Calanog <jan.calanog@elastic.co>
2025-02-07 16:39:38 +0100

    Add pages navigation

--- src/Elastic.Markdown/Assets/main.ts
+++ src/Elastic.Markdown/Assets/main.ts
@@ -1,5 +1,6 @@
 import hljs from "highlight.js";
 import {mergeHTMLPlugin} from "./hljs-merge-html-plugin";
+import {$, $$} from 'select-dom/strict.js'
 
 hljs.registerLanguage('apiheader', function() {
     return {
@@ -16,3 +17,41 @@ hljs.registerLanguage('apiheader', function() {
 
 hljs.addPlugin(mergeHTMLPlugin);
 hljs.highlightAll();
+
+
+type NavState = { [key: string]: boolean };
+
+
+function keepNavState(element: HTMLElement) {
+    const inputs = $$('input[type="checkbox"]', element);
+    const sessionState = JSON.parse(sessionStorage.getItem('pagesNavState')) as NavState
+    if (sessionState) {
+        inputs.forEach(input => {
+            const key = input.id;
+            input.checked = input.checked || sessionState[key];
+        });
+    }
+    window.addEventListener('beforeunload', () => {
+        const state = inputs.reduce((state: NavState, input) => {
+            const key = input.id;
+            const value = input.checked;
+            return { ...state, [key]: value};
+        }, {});
+        sessionStorage.setItem('pagesNavState', JSON.stringify(state));
+    });
+}
+
+function keepNavPosition(element: HTMLElement) {
+    const scrollPosition = sessionStorage.getItem('pagesNavScrollPosition');
+    if (scrollPosition) {
+        element.scrollTop = parseInt(scrollPosition);
+    }
+    window.addEventListener('beforeunload', () => {
+        sessionStorage.setItem('pagesNavScrollPosition', element.scrollTop.toString());
+    });
+}
+
+const pagesNav = $('#pages-nav');
+
+keepNavState(pagesNav);
+keepNavPosition(pagesNav);
