802e0faddcf33cfcbf8c6e8a2e5955ecb94951c3
Martijn Laarman <Mpdreamz@gmail.com>
2025-02-10 21:42:01 +0100

    fix test that we made error on purpose

--- tests/authoring/Blocks/Lists.fs
+++ tests/authoring/Blocks/Lists.fs
@@ -21,7 +21,7 @@ type ``supports loose lists`` () =
         <ul>
             <li>
                 <p>
-                    <strong>onsumption-based billing</strong>:</p>
+                    <strong>Consumption-based billing</strong>:</p>
                 <p>You pay for the actual product used, regardless of the application or use case. This is different from subscription-based billing models where customers pay a flat fee restricted by usage quotas, or one-time upfront payment billing models such as those used for on-prem software licenses.</p>
                 <p>You can purchase credits for a single or multi-year contract. Consumption is on demand, and every month we deduct from your balance based on your usage and contract terms. This allows you to seamlessly expand your usage to the full extent of your requirements and available budget, without any quotas or restrictions.</p>
             </li>
