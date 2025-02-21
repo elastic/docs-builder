0e5fca8d9a66dbb5ad39c4ed7e3f495868e596b4
Jan Calanog <jan.calanog@elastic.co>
2025-02-21 23:18:37 +0100

    Remove unused imports

--- src/Elastic.Markdown/Myst/Directives/TabSetBlock.cs
+++ src/Elastic.Markdown/Myst/Directives/TabSetBlock.cs
@@ -2,11 +2,8 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System.Linq;
-using System.Reflection.Metadata;
 using Elastic.Markdown.Diagnostics;
 using Elastic.Markdown.Helpers;
-using Markdig.Syntax;
 
 namespace Elastic.Markdown.Myst.Directives;
 
