0f109dceb3f97bddae0c3fc25fb172911bb0b69f
Jan Calanog <jan.calanog@elastic.co>
2025-02-27 13:19:01 +0100

    Fix unintended merge changes

--- src/Elastic.Markdown/IO/Navigation/DocumentationGroup.cs
+++ src/Elastic.Markdown/IO/Navigation/DocumentationGroup.cs
@@ -56,7 +56,7 @@ public class DocumentationGroup
 		IDictionary<string, DocumentationFile> lookup,
 		IDictionary<string, DocumentationFile[]> folderLookup,
 		ref int fileIndex,
-		int depth = -1,
+		int depth = 0,
 		MarkdownFile? index = null)
 	{
 		Depth = depth;
