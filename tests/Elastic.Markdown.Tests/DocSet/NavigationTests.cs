28c742a08fa201a43d2c58630563770b3ce13e39
Felipe Cotti <felipe.cotti@elastic.co>
2025-03-14 18:28:40 -0300

    Undo NavigationTests as they were working as intended.

--- tests/Elastic.Markdown.Tests/DocSet/NavigationTests.cs
+++ tests/Elastic.Markdown.Tests/DocSet/NavigationTests.cs
@@ -31,24 +31,24 @@ public class NavigationTests(ITestOutputHelper output) : NavigationTestsBase(out
 	{
 		Configuration.Redirects.Should()
 			.NotBeNullOrEmpty()
-			.And.ContainKey(Path.Combine("testing", "redirects", "first-page-old.md"))
-			.And.ContainKey(Path.Combine("testing", "redirects", "second-page-old.md"))
-			.And.ContainKey(Path.Combine("testing", "redirects", "4th-page.md"))
-			.And.ContainKey(Path.Combine("testing", "redirects", "third-page.md"));
+			.And.ContainKey("testing/redirects/first-page-old.md")
+			.And.ContainKey("testing/redirects/second-page-old.md")
+			.And.ContainKey("testing/redirects/4th-page.md")
+			.And.ContainKey("testing/redirects/third-page.md");
 
-		var redirect1 = Configuration.Redirects![Path.Combine("testing", "redirects", "first-page-old.md")];
-		redirect1.To.Should().Be(Path.Combine("testing", "redirects", "second-page.md"));
+		var redirect1 = Configuration.Redirects!["testing/redirects/first-page-old.md"];
+		redirect1.To.Should().Be("testing/redirects/second-page.md");
 
-		var redirect2 = Configuration.Redirects![Path.Combine("testing", "redirects", "second-page-old.md")];
+		var redirect2 = Configuration.Redirects!["testing/redirects/second-page-old.md"];
 		redirect2.Many.Should().NotBeNullOrEmpty().And.HaveCount(2);
-		redirect2.Many![0].To.Should().Be(Path.Combine("testing", "redirects", "second-page.md"));
-		redirect2.Many![1].To.Should().Be(Path.Combine("testing", "redirects", "third-page.md"));
+		redirect2.Many![0].To.Should().Be("testing/redirects/second-page.md");
+		redirect2.Many![1].To.Should().Be("testing/redirects/third-page.md");
 		redirect2.To.Should().BeNullOrEmpty();
 
-		var redirect3 = Configuration.Redirects![Path.Combine("testing", "redirects", "third-page.md")];
-		redirect3.To.Should().Be(Path.Combine("testing", "redirects", "third-page.md"));
+		var redirect3 = Configuration.Redirects!["testing/redirects/third-page.md"];
+		redirect3.To.Should().Be("testing/redirects/third-page.md");
 
-		var redirect4 = Configuration.Redirects![Path.Combine("testing", "redirects", "4th-page.md")];
-		redirect4.To.Should().Be(Path.Combine("testing", "redirects", "5th-page.md"));
+		var redirect4 = Configuration.Redirects!["testing/redirects/4th-page.md"];
+		redirect4.To.Should().Be("testing/redirects/5th-page.md");
 	}
 }
