965aa770fcc7b916e93aa72a030cabc335252855
Felipe Cotti <felipe.cotti@elastic.co>
2025-03-20 08:32:11 -0300

    Acquire the branch name as a new variable, keeping github.ref_name as a fallback.

--- src/Elastic.Markdown/IO/Discovery/GitCheckoutInformation.cs
+++ src/Elastic.Markdown/IO/Discovery/GitCheckoutInformation.cs
@@ -68,7 +68,7 @@ public record GitCheckoutInformation
 			branch = branch.Replace("ref: ", string.Empty);
 		}
 		else
-			branch = Environment.GetEnvironmentVariable("GITHUB_REF_NAME") ?? "detached/head";
+			branch = Environment.GetEnvironmentVariable("GITHUB_PR_REF_NAME") ?? Environment.GetEnvironmentVariable("GITHUB_REF_NAME") ?? "detached/head";
 
 		var ini = new IniFile();
 		using var stream = gitConfig.OpenRead();
