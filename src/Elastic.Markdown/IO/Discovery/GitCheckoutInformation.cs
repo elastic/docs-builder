ddccc625faa1dcb4793a4ddfc05145c43ccd9c79
Felipe Cotti <felipe.cotti@elastic.co>
2025-03-20 01:34:04 -0300

    Get branch name from workflow if available

--- src/Elastic.Markdown/IO/Discovery/GitCheckoutInformation.cs
+++ src/Elastic.Markdown/IO/Discovery/GitCheckoutInformation.cs
@@ -68,7 +68,7 @@ public record GitCheckoutInformation
 			branch = branch.Replace("ref: ", string.Empty);
 		}
 		else
-			branch = "detached/head";
+			branch = Environment.GetEnvironmentVariable("GITHUB_REF_NAME") ?? "detached/head";
 
 		var ini = new IniFile();
 		using var stream = gitConfig.OpenRead();
