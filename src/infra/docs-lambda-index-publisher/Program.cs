d2d675550100fdaaf96b89e106b8bc59a2d34340
Jan Calanog <jan.calanog@elastic.co>
2025-04-19 15:32:47 +0200

    fix

--- src/infra/docs-lambda-index-publisher/Program.cs
+++ src/infra/docs-lambda-index-publisher/Program.cs
@@ -30,24 +30,37 @@ await LambdaBootstrapBuilder.Create<SQSEvent>(Handler, new SourceGeneratorLambda
 // await CreateLinkIndex(new AmazonS3Client());
 
 #pragma warning disable CS8321 // Local function is declared but never used
-static async Task<string> Handler(SQSEvent ev, ILambdaContext context)
+static async Task<SQSBatchResponse> Handler(SQSEvent evnt, ILambdaContext context)
 #pragma warning restore CS8321 // Local function is declared but never used
 {
+	var batchItemFailures = new List<SQSBatchResponse.BatchItemFailure>();
+	foreach(var message in evnt.Records)
+	{
+		try
+		{
+			//process your message
+			await ProcessMessageAsync(message, context);
+		}
+		catch (Exception)
+		{
+			//Add failed message identifier to the batchItemFailures list
+			batchItemFailures.Add(new SQSBatchResponse.BatchItemFailure{ItemIdentifier=message.MessageId});
+		}
+	}
+	return new SQSBatchResponse(batchItemFailures);
 	// const int maxRetries = 3;
 	// var retryCount = 0;
 
 	// var sw = Stopwatch.StartNew();
-	await Task.Delay(100);
-	foreach (var record in ev.Records)
-	{
-		context.Logger.LogInformation($"Received message: {record.Body}");
-
-		// Delete the message from the queue
-		var sqsClient = new AmazonSQSClient();
-		var deleteMessageRequest = new DeleteMessageRequest(record.EventSourceArn, record.ReceiptHandle);
-		_ = await sqsClient.DeleteMessageAsync(deleteMessageRequest);
-	}
-	return "Done";
+	// await Task.Delay(100);
+	// foreach (var record in ev.Records)
+	// {
+	// 	context.Logger.LogInformation($"Received message: {record.Body}");
+	//
+	// 	// Delete the message from the queue
+	// 	var sqsClient = new AmazonSQSClient();
+	//
+	// }
 
 	// while (true)
 	// {
@@ -100,6 +113,17 @@ static async Task<string> Handler(SQSEvent ev, ILambdaContext context)
 	// 		await Task.Delay(TimeSpan.FromSeconds(1));
 	// 	}
 	// }
+}
+
+static async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+{
+	if (string.IsNullOrEmpty(message.Body))
+	{
+		throw new Exception("No Body in SQS Message.");
+	}
+	context.Logger.LogInformation($"Processed message {message.Body}");
+	// TODO: Do interesting work based on the new message
+	await Task.CompletedTask;
 }
 
 // static async Task<LinkIndex?> CreateLinkIndex(IAmazonS3 s3Client)
