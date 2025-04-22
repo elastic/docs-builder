b6d66fb107f63cb6ca42930bdcd6605870e8a832
Colleen McGinnis <colleen.mcginnis@elastic.co>
2025-04-22 09:25:44 -0500

    reorder imports

--- src/Elastic.Markdown/Myst/Directives/ImageBlock.cs
+++ src/Elastic.Markdown/Myst/Directives/ImageBlock.cs
@@ -2,8 +2,8 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using Elastic.Markdown.Helpers;
 using Elastic.Markdown.Diagnostics;
+using Elastic.Markdown.Helpers;
 using Elastic.Markdown.IO;
 
 namespace Elastic.Markdown.Myst.Directives;
