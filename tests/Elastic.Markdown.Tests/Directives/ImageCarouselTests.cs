61151260534495db2c30adf3252e306334350320
Kylie Meli <kylie.geller@gmail.com>
2025-07-17 08:29:05 -0400

    Adding support to the docs for an image carousel (#1396)
    * who knows
    
    * closing tags fix
    
    * simplify carousel block code
    
    * more cleanup
    
    * adding fixed height setting and removing sample carousel addition
    
    * prettier updates
    
    * adding to the documentation
    
    * use existing pics in the carousel docs
    
    * dropping id
    
    * removed options for controls and indicators
    
    * updated fixed-height
    
    * cleanup and tests
    
    * cleanup
    
    * Update main.ts - fixing conflict resolution
    
    * formatted
    
    * fixing bad conflict resolution
    
    ---------
    
    Co-authored-by: Martijn Laarman <Mpdreamz@gmail.com>

---
+++ tests/Elastic.Markdown.Tests/Directives/ImageCarouselTests.cs
@@ -0,0 +1,210 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.IO.Abstractions.TestingHelpers;
+using Elastic.Documentation.Diagnostics;
+using Elastic.Markdown.Myst.Directives;
+using Elastic.Markdown.Myst.Directives.Image;
+using FluentAssertions;
+
+namespace Elastic.Markdown.Tests.Directives;
+
+public class ImageCarouselBlockTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+:fixed-height: medium
+
+```{image} img/image1.png
+:alt: First image
+```
+
+```{image} img/image2.png
+:alt: Second image
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem)
+	{
+		fileSystem.AddFile(@"docs/img/image1.png", "");
+		fileSystem.AddFile(@"docs/img/image2.png", "");
+	}
+
+	[Fact]
+	public void ParsesBlock() => Block.Should().NotBeNull();
+
+	[Fact]
+	public void ParsesCarouselProperties()
+	{
+		Block!.FixedHeight.Should().Be("medium");
+	}
+
+	[Fact]
+	public void ProcessesNestedImages()
+	{
+		Block!.Images.Should().HaveCount(2);
+		Block!.Images[0].Alt.Should().Be("First image");
+		Block!.Images[0].ImageUrl.Should().Be("/img/image1.png");
+		Block!.Images[1].Alt.Should().Be("Second image");
+		Block!.Images[1].ImageUrl.Should().Be("/img/image2.png");
+	}
+
+	[Fact]
+	public void AllImagesFoundSoNoErrorIsEmitted()
+	{
+		Block!.Images.Should().AllSatisfy(img => img.Found.Should().BeTrue());
+		Collector.Diagnostics.Count.Should().Be(0);
+	}
+}
+
+public class ImageCarouselWithSmallHeightTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+:fixed-height: small
+
+```{image} img/small.png
+:alt: Small image
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem) =>
+		fileSystem.AddFile(@"docs/img/small.png", "");
+
+	[Fact]
+	public void ParsesSmallFixedHeight()
+	{
+		Block!.FixedHeight.Should().Be("small");
+		Collector.Diagnostics.Count.Should().Be(0);
+	}
+}
+
+public class ImageCarouselWithAutoHeightTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+:fixed-height: auto
+
+```{image} img/auto.png
+:alt: Auto height image
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem) =>
+		fileSystem.AddFile(@"docs/img/auto.png", "");
+
+	[Fact]
+	public void ParsesAutoFixedHeight()
+	{
+		Block!.FixedHeight.Should().Be("auto");
+		Collector.Diagnostics.Count.Should().Be(0);
+	}
+}
+
+public class ImageCarouselWithInvalidHeightTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+:fixed-height: large
+
+```{image} img/invalid.png
+:alt: Invalid height image
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem) =>
+		fileSystem.AddFile(@"docs/img/invalid.png", "");
+
+	[Fact]
+	public void WarnsOnInvalidFixedHeight()
+	{
+		Block!.FixedHeight.Should().Be("large");
+
+		Collector.Diagnostics.Should().HaveCount(1)
+			.And.OnlyContain(d => d.Severity == Severity.Warning);
+
+		var warning = Collector.Diagnostics.First();
+		warning.Message.Should().Contain("Invalid fixed-height value 'large'");
+		warning.Message.Should().Contain("Valid options are: auto, small, medium");
+	}
+}
+
+public class ImageCarouselWithoutImagesTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+:::
+"""
+)
+{
+	[Fact]
+	public void EmitsErrorForEmptyCarousel()
+	{
+		Block!.Images.Should().BeEmpty();
+
+		Collector.Diagnostics.Should().HaveCount(1)
+			.And.OnlyContain(d => d.Severity == Severity.Error);
+
+		var error = Collector.Diagnostics.First();
+		error.Message.Should().Be("carousel directive requires nested image directives");
+	}
+}
+
+public class ImageCarouselMinimalTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+
+```{image} img/minimal.png
+:alt: Minimal carousel
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem) =>
+		fileSystem.AddFile(@"docs/img/minimal.png", "");
+
+	[Fact]
+	public void ParsesMinimalCarousel()
+	{
+		Block!.FixedHeight.Should().BeNull();
+		Block!.Images.Should().HaveCount(1);
+		Block!.Images[0].Alt.Should().Be("Minimal carousel");
+		Collector.Diagnostics.Count.Should().Be(0);
+	}
+}
+
+public class ImageCarouselWithMissingImageTests(ITestOutputHelper output) : DirectiveTest<ImageCarouselBlock>(output,
+"""
+:::{carousel}
+
+```{image} img/missing.png
+:alt: Missing image
+```
+
+```{image} img/exists.png
+:alt: Existing image
+```
+:::
+"""
+)
+{
+	protected override void AddToFileSystem(MockFileSystem fileSystem) =>
+		fileSystem.AddFile(@"docs/img/exists.png", "");
+
+	[Fact]
+	public void HandlesPartiallyMissingImages()
+	{
+		Block!.Images.Should().HaveCount(2);
+		Block!.Images[0].Found.Should().BeFalse(); // missing.png
+		Block!.Images[1].Found.Should().BeTrue();  // exists.png
+
+		// Should have diagnostics for the missing image
+		Collector.Diagnostics.Should().HaveCount(1)
+			.And.OnlyContain(d => d.Severity == Severity.Error);
+	}
+}
