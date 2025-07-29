23fc05a999897360aa9d9b08523820cf5d6ab76c
Martijn Laarman <Mpdreamz@gmail.com>
2025-07-29 19:52:11 +0200

    Fix RootNavigation on MarkdownFile

--- src/tooling/docs-assembler/Navigation/GlobalNavigationHtmlWriter.cs
+++ src/tooling/docs-assembler/Navigation/GlobalNavigationHtmlWriter.cs
@@ -3,13 +3,14 @@
 // See the LICENSE file in the project root for more information
 
 using System.Collections.Concurrent;
+using Elastic.Documentation.Diagnostics;
 using Elastic.Documentation.Site.Navigation;
 using Elastic.Markdown.IO.Navigation;
 using Microsoft.Extensions.Logging;
 
 namespace Documentation.Assembler.Navigation;
 
-public class GlobalNavigationHtmlWriter(ILoggerFactory logFactory, GlobalNavigation globalNavigation) : INavigationHtmlWriter
+public class GlobalNavigationHtmlWriter(ILoggerFactory logFactory, GlobalNavigation globalNavigation, DiagnosticsCollector collector) : INavigationHtmlWriter
 {
 	private readonly ILogger<Program> _logger = logFactory.CreateLogger<Program>();
 
@@ -17,34 +18,30 @@ public class GlobalNavigationHtmlWriter(ILoggerFactory logFactory, GlobalNavigat
 
 	public async Task<NavigationRenderResult> RenderNavigation(IRootNavigationItem<INavigationModel, INavigationItem> currentRootNavigation, int maxLevel, Cancel ctx = default)
 	{
-		INodeNavigationItem<INavigationModel, INavigationItem> lastParentBeforeRoot = currentRootNavigation;
-		INodeNavigationItem<INavigationModel, INavigationItem> parent = currentRootNavigation;
-		while (parent.Parent is not null)
-		{
-			lastParentBeforeRoot = parent;
-			parent = parent.Parent;
-		}
-		if (_renderedNavigationCache.TryGetValue((lastParentBeforeRoot.Id, maxLevel), out var html))
+		if (currentRootNavigation.Parent is not null && currentRootNavigation.Parent.Depth != 0)
+			collector.EmitGlobalError($"Passed root is not actually a root {currentRootNavigation.NavigationTitle} ({currentRootNavigation.Id}) in {currentRootNavigation.Url}");
+
+		if (_renderedNavigationCache.TryGetValue((currentRootNavigation.Id, maxLevel), out var html))
 		{
 			return new NavigationRenderResult
 			{
 				Html = html,
-				Id = lastParentBeforeRoot.Id
+				Id = currentRootNavigation.Id
 			};
 		}
 
-		_logger.LogInformation("Rendering navigation for {NavigationTitle} ({Id})", lastParentBeforeRoot.NavigationTitle, lastParentBeforeRoot.Id);
+		_logger.LogInformation("Rendering navigation for {NavigationTitle} ({Id})", currentRootNavigation.NavigationTitle, currentRootNavigation.Id);
 
-		if (lastParentBeforeRoot is not DocumentationGroup group)
+		if (currentRootNavigation is not DocumentationGroup group)
 			return NavigationRenderResult.Empty;
 
 		var model = CreateNavigationModel(group, maxLevel);
 		html = await ((INavigationHtmlWriter)this).Render(model, ctx);
-		_renderedNavigationCache[(lastParentBeforeRoot.Id, maxLevel)] = html;
+		_renderedNavigationCache[(currentRootNavigation.Id, maxLevel)] = html;
 		return new NavigationRenderResult
 		{
 			Html = html,
-			Id = lastParentBeforeRoot.Id
+			Id = currentRootNavigation.Id
 		};
 	}
 
