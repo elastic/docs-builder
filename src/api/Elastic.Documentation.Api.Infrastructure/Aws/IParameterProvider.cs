484338d6bdf82cea376403dd0f1447c810581d1b
Jan Calanog <jan.calanog@elastic.co>
2025-07-31 23:01:11 +0200

    Set up api (#1662)
    * Set up api
    
    * Fix Dockerfile path
    
    * Refactor naming
    
    * Fix editorconfig path
    
    * Refactor editorconfig
    
    * Revert "Refactor editorconfig"
    
    This reverts commit 9217ac0a391ad92372aa881a28cde9bbc66d0d7e.
    
    * Fix path
    
    * Fix editorconfig glob
    
    * Adjust folder structure
    
    * Refactor how DocumentationWebhost is reusing the proxy
    
    * Revert changes to stepper
    
    * Remove empty file
    
    * Refactor dependency resolution
    
    * Refactor mapping
    
    * Fix logging
    
    * Fix AOT build
    
    * Add services necessary for aws lambda
    
    As seen in https://docs.aws.amazon.com/lambda/latest/dg/csharp-package-asp.html#csharp-package-asp-deploy-minimal
    
    * Revert "Add services necessary for aws lambda"
    
    This reverts commit 7fd8de6493fbd6001c90be65b7c571e904698530.
    
    * Add system prompt to business logic
    
    * Remove dead code

---
+++ src/api/Elastic.Documentation.Api.Infrastructure/Aws/IParameterProvider.cs
@@ -0,0 +1,10 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+namespace Elastic.Documentation.Api.Infrastructure.Aws;
+
+public interface IParameterProvider
+{
+	Task<string> GetParam(string name, bool withDecryption = true, Cancel ctx = default);
+}
---
+++ src/api/Elastic.Documentation.Api.Infrastructure/ServicesExtension.cs
@@ -0,0 +1,123 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.ComponentModel.DataAnnotations;
+using Elastic.Documentation.Api.Core;
+using Elastic.Documentation.Api.Core.AskAi;
+using Elastic.Documentation.Api.Infrastructure.Adapters.AskAi;
+using Elastic.Documentation.Api.Infrastructure.Aws;
+using Elastic.Documentation.Api.Infrastructure.Gcp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NetEscapades.EnumGenerators;
+
+namespace Elastic.Documentation.Api.Infrastructure;
+
+[EnumExtensions]
+public enum AppEnvironment
+{
+	[Display(Name = "dev")] Dev,
+	[Display(Name = "staging")] Staging,
+	[Display(Name = "edge")] Edge,
+	[Display(Name = "prod")] Prod
+}
+
+public class LlmGatewayOptions
+{
+	public string ServiceAccount { get; set; } = string.Empty;
+	public string FunctionUrl { get; set; } = string.Empty;
+	public string TargetAudience { get; set; } = string.Empty;
+}
+
+public static class ServicesExtension
+{
+	private static ILogger? GetLogger(IServiceCollection services)
+	{
+		using var serviceProvider = services.BuildServiceProvider();
+		var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+		return loggerFactory?.CreateLogger(typeof(ServicesExtension));
+	}
+
+	public static void AddElasticDocsApiUsecases(this IServiceCollection services, string? appEnvironment)
+	{
+		if (AppEnvironmentExtensions.TryParse(appEnvironment, out var parsedEnvironment, true))
+		{
+			AddElasticDocsApiUsecases(services, parsedEnvironment);
+		}
+		else
+		{
+			var logger = GetLogger(services);
+			logger?.LogWarning("Unable to parse environment {AppEnvironment} into AppEnvironment. Using default AppEnvironment.Dev", appEnvironment);
+			AddElasticDocsApiUsecases(services, AppEnvironment.Dev);
+		}
+	}
+
+
+	private static void AddElasticDocsApiUsecases(this IServiceCollection services, AppEnvironment appEnvironment)
+	{
+		_ = services.ConfigureHttpJsonOptions(options =>
+		{
+			options.SerializerOptions.TypeInfoResolverChain.Insert(0, ApiJsonContext.Default);
+		});
+		_ = services.AddHttpClient();
+		AddParameterProvider(services, appEnvironment);
+		AddAskAiUsecase(services, appEnvironment);
+	}
+
+	// https://docs.aws.amazon.com/systems-manager/latest/userguide/ps-integration-lambda-extensions.html
+	private static void AddParameterProvider(IServiceCollection services, AppEnvironment appEnvironment)
+	{
+		var logger = GetLogger(services);
+
+		switch (appEnvironment)
+		{
+			case AppEnvironment.Prod:
+			case AppEnvironment.Staging:
+			case AppEnvironment.Edge:
+				{
+					logger?.LogInformation("Configuring LambdaExtensionParameterProvider for environment {AppEnvironment}", appEnvironment);
+					_ = services.AddHttpClient(LambdaExtensionParameterProvider.HttpClientName, client =>
+					{
+						client.BaseAddress = new Uri("http://localhost:2773");
+						client.DefaultRequestHeaders.Add("X-Aws-Parameters-Secrets-Token", Environment.GetEnvironmentVariable("AWS_SESSION_TOKEN"));
+					});
+					_ = services.AddSingleton<IParameterProvider, LambdaExtensionParameterProvider>();
+					break;
+				}
+			case AppEnvironment.Dev:
+				{
+					logger?.LogInformation("Configuring LocalParameterProvider for environment {AppEnvironment}", appEnvironment);
+					_ = services.AddSingleton<IParameterProvider, LocalParameterProvider>();
+					break;
+				}
+			default:
+				{
+					throw new ArgumentOutOfRangeException(nameof(appEnvironment), appEnvironment,
+						"Unsupported environment for parameter provider.");
+				}
+		}
+	}
+
+	private static void AddAskAiUsecase(IServiceCollection services, AppEnvironment appEnvironment)
+	{
+		var logger = GetLogger(services);
+		logger?.LogInformation("Configuring AskAi use case for environment {AppEnvironment}", appEnvironment);
+
+		_ = services.Configure<LlmGatewayOptions>(options =>
+		{
+			var serviceProvider = services.BuildServiceProvider();
+			var parameterProvider = serviceProvider.GetRequiredService<IParameterProvider>();
+			var appEnvString = appEnvironment.ToStringFast(true);
+
+			options.ServiceAccount = parameterProvider.GetParam($"/elastic-docs-v3/{appEnvString}/llm-gateway-service-account").GetAwaiter().GetResult();
+			options.FunctionUrl = parameterProvider.GetParam($"/elastic-docs-v3/{appEnvString}/llm-gateway-function-url").GetAwaiter().GetResult();
+
+			var functionUri = new Uri(options.FunctionUrl);
+			options.TargetAudience = $"{functionUri.Scheme}://{functionUri.Host}";
+		});
+		_ = services.AddScoped<GcpIdTokenProvider>();
+		_ = services.AddScoped<IAskAiGateway<Stream>, LlmGatewayAskAiGateway>();
+		_ = services.AddScoped<AskAiUsecase>();
+	}
+}
