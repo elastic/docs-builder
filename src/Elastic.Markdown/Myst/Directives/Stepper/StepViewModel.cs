055f622b9768957e5922ef1363a9def00c1ff758
Fabrizio Ferri Benedetti <fabri.ferribenedetti@elastic.co>
2025-08-01 11:08:32 +0200

    Use Markdig

--- src/Elastic.Markdown/Myst/Directives/Stepper/StepViewModel.cs
+++ src/Elastic.Markdown/Myst/Directives/Stepper/StepViewModel.cs
@@ -2,6 +2,9 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using Elastic.Markdown.Helpers;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
 using Microsoft.AspNetCore.Html;
 
 namespace Elastic.Markdown.Myst.Directives.Stepper;
@@ -13,7 +16,10 @@ public class StepViewModel : DirectiveViewModel
 	public required int HeadingLevel { get; init; }
 
 	/// <summary>
-	/// Renders the title with substitutions applied
+	/// Renders the title with full markdown processing (substitutions, links, emphasis, etc.)
 	/// </summary>
-	public HtmlString RenderTitle() => new(Title);
+	public HtmlString RenderTitle() =>
+		// For now, just return the title as-is since substitutions are already applied in FinalizeAndValidate
+		// In the future, this could be extended to handle full markdown processing using the HTML renderer
+		new(Title);
 }
