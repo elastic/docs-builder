9dce917f5208faa2ef7fce6655191e3564bb1e08
Jan Calanog <jan.calanog@elastic.co>
2025-08-04 16:14:10 +0200

    Add basic search functionality using a mock endpoint

--- src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/Suggestions.tsx
+++ src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/Suggestions.tsx
@@ -1,19 +1,24 @@
-import { AskAiSuggestions } from './AskAiSuggestions'
-import { SearchSuggestions } from './SearchSuggestions'
+import { AskAiSuggestions } from './AskAi/AskAiSuggestions'
+import { SearchSuggestions } from './Search/SearchSuggestions'
+import { useSearchQuery } from './Search/useSearchQuery'
+import { useSearchTerm } from './search.store'
 import { EuiHorizontalRule } from '@elastic/eui'
 import * as React from 'react'
 
 export function Suggestions() {
+    const searchTerm = useSearchTerm()
     return (
         <>
-            <SearchSuggestions
-                suggestions={[
-                    { title: 'Contribute', url: '/contribute' },
-                    { title: 'Syntax guide', url: '/syntax' },
-                    { title: 'Quick reference', url: '/syntax/quick-ref' },
-                    { title: 'Headings', url: '/syntax/headings' },
-                ]}
-            />
+            {!searchTerm && (
+                <SearchSuggestions
+                    suggestions={[
+                        { title: 'Contribute', url: '/contribute' },
+                        { title: 'Syntax guide', url: '/syntax' },
+                        { title: 'Quick reference', url: '/syntax/quick-ref' },
+                        { title: 'Headings', url: '/syntax/headings' },
+                    ]}
+                />
+            )}
             <EuiHorizontalRule margin="m" />
             <AskAiSuggestions
                 suggestions={[
