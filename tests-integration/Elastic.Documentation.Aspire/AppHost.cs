23c09787a9b86a780ea18e57c59f1d896be5defe
Martijn Laarman <Mpdreamz@gmail.com>
2025-08-13 20:25:53 +0200

    Add Search/AI Assistant to dev enviroment.
    This also exposes the proxy to `docs-builder serve-static`.
    
    It now also wires up the configuration for our dotnet aspire project.

--- tests-integration/Elastic.Documentation.Aspire/AppHost.cs
+++ tests-integration/Elastic.Documentation.Aspire/AppHost.cs
@@ -19,19 +19,39 @@ if (logLevel != LogLevel.Information)
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add a secret parameter named "secret"
+var llmUrl = builder.AddParameter("LlmGatewayUrl", secret: true);
+var llmServiceAccountPath = builder.AddParameter("LlmGatewayServiceAccountPath", secret: true);
+
 var cloneAll = builder.AddProject<Projects.docs_assembler>("DocsAssemblerCloneAll").WithArgs(["repo", "clone-all", .. globalArguments]);
 
 var buildAll = builder.AddProject<Projects.docs_assembler>("DocsAssemblerBuildAll").WithArgs(["repo", "build-all", .. globalArguments])
 	.WaitForCompletion(cloneAll);
 
-var elasticsearch = builder.AddElasticsearch("elasticsearch");
+var elasticsearch = builder.AddElasticsearch("elasticsearch")
+	.WithEnvironment("LICENSE", "trial");
 
 var api = builder.AddProject<Projects.Elastic_Documentation_Api_Lambda>("ApiLambda").WithArgs(globalArguments)
+	.WithEnvironment("ENVIRONMENT", "dev")
+	.WithEnvironment("LLM_GATEWAY_FUNCTION_URL", llmUrl)
+	.WithEnvironment("LLM_GATEWAY_SERVICE_ACCOUNT_KEY_PATH", llmServiceAccountPath)
 	.WaitFor(elasticsearch)
 	.WithReference(elasticsearch);
 
 var indexElasticsearch = builder.AddProject<Projects.docs_assembler>("DocsAssemblerElasticsearch")
-	.WithArgs(["repo", "build-all", "--exporters", "html,elasticsearch", .. globalArguments])
+	.WithArgs(["repo", "build-all", "--exporters", "elasticsearch", .. globalArguments])
+	.WithEnvironment("DOCUMENTATION_ELASTIC_URL", elasticsearch.GetEndpoint("http"))
+	.WithEnvironment(context =>
+	{
+		context.EnvironmentVariables["DOCUMENTATION_ELASTIC_PASSWORD"] = elasticsearch.Resource.PasswordParameter;
+	})
+	.WithReference(elasticsearch)
+	.WithExplicitStart()
+	.WaitFor(elasticsearch)
+	.WaitForCompletion(cloneAll);
+
+var indexElasticsearchSemantic = builder.AddProject<Projects.docs_assembler>("DocsAssemblerElasticsearchSemantic")
+	.WithArgs(["repo", "build-all", "--exporters", "semantic", .. globalArguments])
 	.WithEnvironment("DOCUMENTATION_ELASTIC_URL", elasticsearch.GetEndpoint("http"))
 	.WithEnvironment(context =>
 	{
@@ -44,11 +64,17 @@ var indexElasticsearch = builder.AddProject<Projects.docs_assembler>("DocsAssemb
 
 var serveStatic = builder.AddProject<Projects.docs_builder>("DocsBuilderServeStatic")
 	.WithReference(elasticsearch)
+	.WithEnvironment("LLM_GATEWAY_FUNCTION_URL", llmUrl)
+	.WithEnvironment("LLM_GATEWAY_SERVICE_ACCOUNT_KEY_PATH", llmServiceAccountPath)
+	.WithEnvironment("DOCUMENTATION_ELASTIC_URL", elasticsearch.GetEndpoint("http"))
+	.WithEnvironment(context =>
+	{
+		context.EnvironmentVariables["DOCUMENTATION_ELASTIC_PASSWORD"] = elasticsearch.Resource.PasswordParameter;
+	})
 	.WithHttpEndpoint(port: 4000, isProxied: false)
 	.WithArgs(["serve-static", .. globalArguments])
 	.WithHttpHealthCheck("/", 200)
+	.WaitFor(elasticsearch)
 	.WaitForCompletion(buildAll);
-
-
 
 builder.Build().Run();
