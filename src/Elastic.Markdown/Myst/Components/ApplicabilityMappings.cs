3c1f7ad289a37e80fe9e881def5cb84fb5e48b74
Jan Calanog <jan.calanog@elastic.co>
2025-09-12 11:51:18 +0200

    Refactor applies_to

---
+++ src/Elastic.Markdown/Myst/Components/ApplicabilityMappings.cs
@@ -0,0 +1,64 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using Elastic.Documentation.Configuration.Versions;
+
+namespace Elastic.Markdown.Myst.Components;
+
+public static class ApplicabilityMappings
+{
+	public record ApplicabilityDefinition(string Key, string DisplayName, VersioningSystemId VersioningSystemId);
+
+	private static readonly Dictionary<string, ApplicabilityDefinition> Mappings = new()
+	{
+		// Stack
+		["stack"] = new ApplicabilityDefinition("Stack", "Elastic&nbsp;Stack", VersioningSystemId.Stack),
+
+		// Serverless
+		["serverless"] = new ApplicabilityDefinition("Serverless", "Elastic&nbsp;Cloud&nbsp;Serverless", VersioningSystemId.Serverless),
+		["serverless-elasticsearch"] = new ApplicabilityDefinition("Serverless Elasticsearch", "Serverless&nbsp;Elasticsearch projects", VersioningSystemId.ElasticsearchProject),
+		["serverless-observability"] = new ApplicabilityDefinition("Serverless Observability", "Serverless&nbsp;Observability projects", VersioningSystemId.ObservabilityProject),
+		["serverless-security"] = new ApplicabilityDefinition("Serverless Security", "Serverless&nbsp;Security projects", VersioningSystemId.SecurityProject),
+
+		// Deployment
+		["ech"] = new ApplicabilityDefinition("ECH", "Elastic&nbsp;Cloud&nbsp;Hosted", VersioningSystemId.Ess),
+		["eck"] = new ApplicabilityDefinition("ECK", "Elastic&nbsp;Cloud&nbsp;on&nbsp;Kubernetes", VersioningSystemId.Eck),
+		["ece"] = new ApplicabilityDefinition("ECE", "Elastic&nbsp;Cloud&nbsp;Enterprise", VersioningSystemId.Ece),
+		["self"] = new ApplicabilityDefinition("Self-Managed", "Self-managed Elastic&nbsp;deployments", VersioningSystemId.Self),
+
+		// Product Applicability
+		["ecctl"] = new ApplicabilityDefinition("ECCTL", "Elastic&nbsp;Cloud&nbsp;Control", VersioningSystemId.Ecctl),
+		["curator"] = new ApplicabilityDefinition("Curator", "Curator", VersioningSystemId.Curator),
+
+		// EDOT Products
+		["edot-android"] = new ApplicabilityDefinition("EDOT Android", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;Android", VersioningSystemId.EdotAndroid),
+		["edot-cf-aws"] = new ApplicabilityDefinition("EDOT CF AWS", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;Cloud&nbsp;Forwarder for AWS", VersioningSystemId.EdotCfAws),
+		["edot-collector"] = new ApplicabilityDefinition("EDOT Collector", "Elastic Distribution of OpenTelemetry Collector", VersioningSystemId.EdotCollector),
+		["edot-dotnet"] = new ApplicabilityDefinition("EDOT .NET", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;.NET", VersioningSystemId.EdotDotnet),
+		["edot-ios"] = new ApplicabilityDefinition("EDOT iOS", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;iOS", VersioningSystemId.EdotIos),
+		["edot-java"] = new ApplicabilityDefinition("EDOT Java", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;Java", VersioningSystemId.EdotJava),
+		["edot-node"] = new ApplicabilityDefinition("EDOT Node.js", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;Node.js", VersioningSystemId.EdotNode),
+		["edot-php"] = new ApplicabilityDefinition("EDOT PHP", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;PHP", VersioningSystemId.ApmAgentPhp),
+		["edot-python"] = new ApplicabilityDefinition("EDOT Python", "Elastic&nbsp;Distribution of OpenTelemetry&nbsp;Python", VersioningSystemId.EdotPython),
+
+		// APM Agents
+		["apm-agent-android"] = new ApplicabilityDefinition("APM Agent Android", "Application&nbsp;Performance&nbsp;Monitoring Agent for Android", VersioningSystemId.ApmAgentAndroid),
+		["apm-agent-dotnet"] = new ApplicabilityDefinition("APM Agent .NET", "Application&nbsp;Performance&nbsp;Monitoring Agent for .NET", VersioningSystemId.ApmAgentDotnet),
+		["apm-agent-go"] = new ApplicabilityDefinition("APM Agent Go", "Application&nbsp;Performance&nbsp;Monitoring Agent for Go", VersioningSystemId.ApmAgentGo),
+		["apm-agent-ios"] = new ApplicabilityDefinition("APM Agent iOS", "Application&nbsp;Performance&nbsp;Monitoring Agent for iOS", VersioningSystemId.ApmAgentIos),
+		["apm-agent-java"] = new ApplicabilityDefinition("APM Agent Java", "Application&nbsp;Performance&nbsp;Monitoring Agent for Java", VersioningSystemId.ApmAgentJava),
+		["apm-agent-node"] = new ApplicabilityDefinition("APM Agent Node.js", "Application&nbsp;Performance&nbsp;Monitoring Agent for Node.js", VersioningSystemId.ApmAgentNode),
+		["apm-agent-php"] = new ApplicabilityDefinition("APM Agent PHP", "Application&nbsp;Performance&nbsp;Monitoring Agent for PHP", VersioningSystemId.ApmAgentPhp),
+		["apm-agent-python"] = new ApplicabilityDefinition("APM Agent Python", "Application&nbsp;Performance&nbsp;Monitoring Agent for Python", VersioningSystemId.ApmAgentPython),
+		["apm-agent-ruby"] = new ApplicabilityDefinition("APM Agent Ruby", "Application&nbsp;Performance&nbsp;Monitoring Agent for Ruby", VersioningSystemId.ApmAgentRuby),
+		["apm-agent-rum"] = new ApplicabilityDefinition("APM Agent RUM", "Application&nbsp;Performance&nbsp;Monitoring Agent for Real&nbsp;User&nbsp;Monitoring", VersioningSystemId.ApmAgentRum),
+
+		// Generic product
+		["product"] = new ApplicabilityDefinition("", "", VersioningSystemId.All)
+	};
+
+	public static ApplicabilityDefinition GetProductDefinition(string productKey) => Mappings.TryGetValue(productKey, out var definition)
+			? definition
+			: throw new ArgumentException($"Unknown product key: {productKey}");
+}
--- src/Elastic.Markdown/Myst/Components/ApplicableToViewModel.cs
+++ src/Elastic.Markdown/Myst/Components/ApplicableToViewModel.cs
@@ -9,7 +9,176 @@ namespace Elastic.Markdown.Myst.Components;
 
 public class ApplicableToViewModel
 {
+	private readonly ApplicabilityRenderer _applicabilityRenderer = new();
+
 	public required bool Inline { get; init; }
 	public required ApplicableTo AppliesTo { get; init; }
 	public required VersionsConfiguration VersionsConfig { get; init; }
+
+	public IEnumerable<ApplicabilityItem> GetApplicabilityItems()
+	{
+		var items = new List<ApplicabilityItem>();
+
+		// Process Stack
+		if (AppliesTo.Stack is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("stack");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(AppliesTo.Stack, productDef, versioningSystem));
+		}
+
+		// Process Serverless
+		if (AppliesTo.Serverless is not null)
+		{
+			if (AppliesTo.Serverless.AllProjects is not null)
+			{
+				var productDef = ApplicabilityMappings.GetProductDefinition("serverless");
+				var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+				items.AddRange(ProcessApplicabilityCollection(AppliesTo.Serverless.AllProjects, productDef, versioningSystem));
+			}
+			else
+				items.AddRange(ProcessServerlessProjects(AppliesTo.Serverless));
+		}
+
+		// Process Deployment
+		if (AppliesTo.Deployment is not null)
+			items.AddRange(ProcessDeploymentTypes(AppliesTo.Deployment));
+
+		// Process Product Applicability
+		if (AppliesTo.ProductApplicability is not null)
+			items.AddRange(ProcessProductApplicability(AppliesTo.ProductApplicability));
+
+		// Process Generic Product
+		if (AppliesTo.Product is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("product");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(AppliesTo.Product, productDef, versioningSystem));
+		}
+
+		return items;
+	}
+
+	private IEnumerable<ApplicabilityItem> ProcessServerlessProjects(ServerlessProjectApplicability serverless)
+	{
+		var items = new List<ApplicabilityItem>();
+
+		if (serverless.Elasticsearch is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("serverless-elasticsearch");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(serverless.Elasticsearch, productDef, versioningSystem));
+		}
+
+		if (serverless.Observability is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("serverless-observability");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(serverless.Observability, productDef, versioningSystem));
+		}
+
+		if (serverless.Security is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("serverless-security");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(serverless.Security, productDef, versioningSystem));
+		}
+
+		return items;
+	}
+
+	private IEnumerable<ApplicabilityItem> ProcessDeploymentTypes(DeploymentApplicability deployment)
+	{
+		var items = new List<ApplicabilityItem>();
+
+		if (deployment.Ess is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("ech");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(deployment.Ess, productDef, versioningSystem));
+		}
+
+		if (deployment.Eck is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("eck");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(deployment.Eck, productDef, versioningSystem));
+		}
+
+		if (deployment.Ece is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("ece");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(deployment.Ece, productDef, versioningSystem));
+		}
+
+		if (deployment.Self is not null)
+		{
+			var productDef = ApplicabilityMappings.GetProductDefinition("self");
+			var versioningSystem = VersionsConfig.GetVersioningSystem(productDef.VersioningSystemId);
+			items.AddRange(ProcessApplicabilityCollection(deployment.Self, productDef, versioningSystem));
+		}
+
+		return items;
+	}
+
+	private IEnumerable<ApplicabilityItem> ProcessProductApplicability(ProductApplicability productApplicability)
+	{
+		var items = new List<ApplicabilityItem>();
+
+		// Process each product applicability property explicitly (AOT-compatible)
+		ProcessProductIfNotNull(productApplicability.Ecctl, "ecctl", items);
+		ProcessProductIfNotNull(productApplicability.Curator, "curator", items);
+		ProcessProductIfNotNull(productApplicability.EdotAndroid, "edot-android", items);
+		ProcessProductIfNotNull(productApplicability.EdotCfAws, "edot-cf-aws", items);
+		ProcessProductIfNotNull(productApplicability.EdotCollector, "edot-collector", items);
+		ProcessProductIfNotNull(productApplicability.EdotDotnet, "edot-dotnet", items);
+		ProcessProductIfNotNull(productApplicability.EdotIos, "edot-ios", items);
+		ProcessProductIfNotNull(productApplicability.EdotJava, "edot-java", items);
+		ProcessProductIfNotNull(productApplicability.EdotNode, "edot-node", items);
+		ProcessProductIfNotNull(productApplicability.EdotPhp, "edot-php", items);
+		ProcessProductIfNotNull(productApplicability.EdotPython, "edot-python", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentAndroid, "apm-agent-android", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentDotnet, "apm-agent-dotnet", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentGo, "apm-agent-go", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentIos, "apm-agent-ios", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentJava, "apm-agent-java", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentNode, "apm-agent-node", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentPhp, "apm-agent-php", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentPython, "apm-agent-python", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentRuby, "apm-agent-ruby", items);
+		ProcessProductIfNotNull(productApplicability.ApmAgentRum, "apm-agent-rum", items);
+
+		return items;
+	}
+
+	private void ProcessProductIfNotNull(AppliesCollection? collection, string productKey, List<ApplicabilityItem> items)
+	{
+		if (collection is null)
+			return;
+
+		var applicabilityDefinition = ApplicabilityMappings.GetProductDefinition(productKey);
+		var versioningSystem = VersionsConfig.GetVersioningSystem(applicabilityDefinition.VersioningSystemId);
+		items.AddRange(ProcessApplicabilityCollection(collection, applicabilityDefinition, versioningSystem));
+	}
+
+	private IEnumerable<ApplicabilityItem> ProcessApplicabilityCollection(
+		AppliesCollection applications,
+		ApplicabilityMappings.ApplicabilityDefinition applicabilityDefinition,
+		VersioningSystem versioningSystem) =>
+		applications.Select(applicability =>
+		{
+			var renderData = _applicabilityRenderer.RenderApplicability(
+				applicability,
+				applicabilityDefinition,
+				versioningSystem,
+				applications);
+
+			return new ApplicabilityItem(
+				Key: applicabilityDefinition.Key,
+				Applicability: applicability,
+				RenderData: renderData
+			);
+		});
+
 }
