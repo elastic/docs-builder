4b20c135ecb9dd16bb63a3ad9a6ebf88ddee632d
Jan Calanog <jan.calanog@elastic.co>
2025-09-22 12:36:28 +0200

    Update src/Elastic.Documentation.Site/Assets/markdown/applies-to.ts
    Co-authored-by: Martijn Laarman <Mpdreamz@gmail.com>

--- src/Elastic.Documentation.Site/Assets/markdown/applies-to.ts
+++ src/Elastic.Documentation.Site/Assets/markdown/applies-to.ts
@@ -26,14 +26,4 @@ document.addEventListener('htmx:load', function () {
         )
     })
     
-    // tippy(
-    //     [
-    //         '.applies [data-tippy-content]:not([data-tippy-content=""])',
-    //         '.applies-inline [data-tippy-content]:not([data-tippy-content=""])',
-    //     ].join(', '),
-    //     {
-    //         delay: [400, 100],
-    //         hideOnClick: false,
-    //     }
-    // )
 })
