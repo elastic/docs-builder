6ee6ad0443ba470c76688fcf05d473a941b0ebd6
Jan Calanog <jan.calanog@elastic.co>
2025-09-22 12:37:01 +0200

    Run prettier

--- src/Elastic.Documentation.Site/Assets/markdown/applies-to.ts
+++ src/Elastic.Documentation.Site/Assets/markdown/applies-to.ts
@@ -1,29 +1,23 @@
-import tippy from 'tippy.js'
 import { $$ } from 'select-dom'
+import tippy from 'tippy.js'
 
 document.addEventListener('htmx:load', function () {
-    
     const selector = [
         '.applies [data-tippy-content]:not([data-tippy-content=""])',
         '.applies-inline [data-tippy-content]:not([data-tippy-content=""])',
     ].join(', ')
-    
+
     const appliesToBadgesWithTooltip = $$(selector)
-    appliesToBadgesWithTooltip.forEach(badge => {
+    appliesToBadgesWithTooltip.forEach((badge) => {
         const content = badge.getAttribute('data-tippy-content')
-        if (!content) 
-            return
-        tippy(
-            badge,
-            {
-                content,
-                allowHTML: true,
-                delay: [400, 100],
-                hideOnClick: false,
-                ignoreAttributes: true,
-                theme: 'applies-to'
-            }
-        )
+        if (!content) return
+        tippy(badge, {
+            content,
+            allowHTML: true,
+            delay: [400, 100],
+            hideOnClick: false,
+            ignoreAttributes: true,
+            theme: 'applies-to',
+        })
     })
-    
 })
