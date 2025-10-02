e925652227617dde35dd971557d19493a493e179
Fabrizio Ferri Benedetti <fabri.ferribenedetti@elastic.co>
2025-10-02 11:44:41 +0200

    Add hints

--- src/tooling/docs-builder/Diagnostics/LiveMode/LiveModeDiagnosticsCollector.cs
+++ src/tooling/docs-builder/Diagnostics/LiveMode/LiveModeDiagnosticsCollector.cs
@@ -12,7 +12,27 @@ namespace Documentation.Builder.Diagnostics.LiveMode;
 public class LiveModeDiagnosticsCollector(ILoggerFactory logFactory)
 	: DiagnosticsCollector([new Log(logFactory.CreateLogger<Log>())])
 {
-	protected override void HandleItem(Diagnostic diagnostic) { }
+	private readonly List<Diagnostic> _errors = [];
+	private readonly List<Diagnostic> _warnings = [];
+	private readonly List<Diagnostic> _hints = [];
 
-	public override async Task StopAsync(Cancel cancellationToken) => await Task.CompletedTask;
+	protected override void HandleItem(Diagnostic diagnostic)
+	{
+		if (diagnostic.Severity == Severity.Error)
+			_errors.Add(diagnostic);
+		else if (diagnostic.Severity == Severity.Warning)
+			_warnings.Add(diagnostic);
+		else
+			_hints.Add(diagnostic);
+	}
+
+	public override async Task StopAsync(Cancel cancellationToken)
+	{
+		if (_errors.Count > 0 || _warnings.Count > 0 || _hints.Count > 0)
+		{
+			var repository = new Elastic.Documentation.Tooling.Diagnostics.Console.ErrataFileSourceRepository();
+			repository.WriteDiagnosticsToConsole(_errors, _warnings, _hints);
+		}
+		await Task.CompletedTask;
+	}
 }
