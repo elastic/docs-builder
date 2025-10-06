0597595020e3ed9d6e037e3ca29994ecbd5bd333
Jan Calanog <jan.calanog@elastic.co>
2025-10-07 00:07:35 +0200

    Revert Markdown.cs

--- src/Elastic.Markdown/Helpers/Markdown.cs
+++ src/Elastic.Markdown/Helpers/Markdown.cs
@@ -2,11 +2,9 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
-using System.Text.RegularExpressions;
-
 namespace Elastic.Markdown.Helpers;
 
-public static partial class MarkdownStringExtensions
+public static class MarkdownStringExtensions
 {
 	public static string StripMarkdown(this string markdown)
 	{
