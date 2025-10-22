442d00db07d8b7d673dfa9711cbf111712ed587e
Felipe Cotti <felipe.cotti@elastic.co>
2025-10-22 12:01:14 -0300

    Lint

--- src/Elastic.Markdown/Exporters/Elasticsearch/ElasticsearchMarkdownExporter.cs
+++ src/Elastic.Markdown/Exporters/Elasticsearch/ElasticsearchMarkdownExporter.cs
@@ -3,9 +3,9 @@
 // See the LICENSE file in the project root for more information
 
 using System.IO.Abstractions;
-using Elastic.Documentation.AppliesTo;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Elastic.Documentation.AppliesTo;
 using Elastic.Documentation.Configuration;
 using Elastic.Documentation.Configuration.Synonyms;
 using Elastic.Documentation.Diagnostics;
