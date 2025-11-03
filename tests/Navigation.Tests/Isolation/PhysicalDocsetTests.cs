eb735f8a9b02cff1c721b995e14b5b76e7337aa7
Martijn Laarman <Mpdreamz@gmail.com>
2025-11-03 14:23:34 +0100

    fix test

--- tests/Navigation.Tests/Isolation/PhysicalDocsetTests.cs
+++ tests/Navigation.Tests/Isolation/PhysicalDocsetTests.cs
@@ -122,10 +122,10 @@ public class PhysicalDocsetTests(ITestOutputHelper output)
 		var developmentToc = tocNavs.FirstOrDefault(t => t.Url == "/development/");
 		developmentToc.Should().NotBeNull();
 
-		developmentToc.NavigationItems.Should().HaveCount(2);
+		developmentToc.NavigationItems.Should().HaveCount(3);
 		developmentToc.Index.Should().NotBeNull();
 		developmentToc.NavigationItems.OfType<FileNavigationLeaf<TestDocumentationFile>>().Should().HaveCount(0);
-		developmentToc.NavigationItems.OfType<FolderNavigation<TestDocumentationFile>>().Should().HaveCount(1);
+		developmentToc.NavigationItems.OfType<FolderNavigation<TestDocumentationFile>>().Should().HaveCount(2);
 		developmentToc.NavigationItems.OfType<TableOfContentsNavigation<TestDocumentationFile>>().Should().HaveCount(1);
 
 		var developmentIndex = developmentToc.Index as FileNavigationLeaf<TestDocumentationFile>;
