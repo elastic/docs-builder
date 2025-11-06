0df3bcc6118dff7ac008a64a63f24feb22063b92
Jan Calanog <jan.calanog@elastic.co>
2025-11-06 09:24:03 +0100

    Fix typescript errors and add CI check (#2170)
    * Add ci check
    
    * Fix all typescript errors
    
    * Fix lint errors
    
    * Prettier

--- src/Elastic.Documentation.Site/Assets/smooth-scroll.ts
+++ src/Elastic.Documentation.Site/Assets/smooth-scroll.ts
@@ -3,9 +3,9 @@ import { $$ } from 'select-dom'
 export function initSmoothScroll() {
     $$('#markdown-content a[href^="#"]').forEach((el) => {
         el.addEventListener('click', (e) => {
-            const target = document.getElementById(
-                el.getAttribute('href').slice(1)
-            )
+            // Using ! because href is guaranteed to be present due to the selector
+            const id = el.getAttribute('href')!.slice(1) // remove the '#' character
+            const target = document.getElementById(id)
             if (target) {
                 e.preventDefault()
                 target.scrollIntoView({ behavior: 'smooth' })
