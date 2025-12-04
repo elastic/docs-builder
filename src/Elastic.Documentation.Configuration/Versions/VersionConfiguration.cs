cfea94360be5dbea122ce421c664eb84e62d5a1f
lcawl <lcawley@elastic.co>
2025-12-04 12:04:59 -0800

    Update VersionConfiguration.cs

--- src/Elastic.Documentation.Configuration/Versions/VersionConfiguration.cs
+++ src/Elastic.Documentation.Configuration/Versions/VersionConfiguration.cs
@@ -121,6 +121,22 @@ public enum VersioningSystemId
 	SearchUI,
 	[Display(Name = "cloud-terraform")]
 	CloudTerraform,
+	[Display(Name = "elasticsearch-client-go")]
+	ElasticsearchClientGo,
+	[Display(Name = "elasticsearch-client-java")]
+	ElasticsearchClientJava,
+	[Display(Name = "elasticsearch-client-javascript")]
+	ElasticsearchClientJavascript,
+	[Display(Name = "elasticsearch-client-dotnet")]
+	ElasticsearchClientDotnet,
+	[Display(Name = "elasticsearch-client-php")]
+	ElasticsearchClientPhp,
+	[Display(Name = "elasticsearch-client-python")]
+	ElasticsearchClientPython,
+	[Display(Name = "elasticsearch-client-ruby")]
+	ElasticsearchClientRuby,
+	[Display(Name = "elasticsearch-client-rust")]
+	ElasticsearchClientRust,
 }
 
 [YamlSerializable]
