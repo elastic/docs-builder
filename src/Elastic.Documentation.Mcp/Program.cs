5266844918a79adaa3681ef3c76bf8161107cbd2
Jan Calanog <jan.calanog@elastic.co>
2025-12-17 10:42:42 +0100

    Register LinksIndexCrossLinkFetcher in DI container and inject properly (#2376)

--- src/Elastic.Documentation.Mcp/Program.cs
+++ src/Elastic.Documentation.Mcp/Program.cs
@@ -2,6 +2,8 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using Elastic.Documentation.LinkIndex;
+using Elastic.Documentation.Links.InboundLinks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -10,10 +12,12 @@ var builder = Host.CreateApplicationBuilder(args);
 builder.Logging.AddConsole(options =>
 	options.LogToStandardErrorThreshold = LogLevel.Trace);
 
+builder.Services.AddSingleton<ILinkIndexReader>(_ => Aws3LinkIndexReader.CreateAnonymous());
+builder.Services.AddSingleton<LinksIndexCrossLinkFetcher>();
+
 builder.Services
 	.AddMcpServer()
 	.WithStdioServerTransport()
 	.WithToolsFromAssembly();
 
 await builder.Build().RunAsync();
-
