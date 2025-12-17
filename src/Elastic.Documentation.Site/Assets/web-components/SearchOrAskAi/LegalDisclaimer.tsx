77913edbc143b5eea9f6f93f1103981128640871
Jan Calanog <jan.calanog@elastic.co>
2025-12-17 18:34:40 +0100

    Add legal disclaimer and reposition empty chat components

---
+++ src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/LegalDisclaimer.tsx
@@ -0,0 +1,31 @@
+import { useEuiTheme } from '@elastic/eui'
+import { css, SerializedStyles } from '@emotion/react'
+
+interface LegalDisclaimerProps {
+    css?: SerializedStyles
+}
+
+export const LegalDisclaimer = ({
+    css: customCss,
+}: LegalDisclaimerProps = {}) => {
+    const { euiTheme } = useEuiTheme()
+    return (
+        <div
+            css={[
+                css`
+                    text-align: center;
+                    color: ${euiTheme.colors.textDisabled};
+                    font-size: 10px;
+                `,
+                customCss,
+            ]}
+        >
+            This chatbot uses AI and can make mistakes, so review the
+            information for accuracy. Your queries and interactions may be
+            stored to help improve and train the model. Do not enter personal,
+            confidential, or proprietary information. Any personal data
+            collected will be handled according to our General Privacy
+            Statement.
+        </div>
+    )
+}
