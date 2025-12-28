261949805b1c665268e0d1ac74d321110a10259b
Alex Cerezo <hello.alexcerezo@gmail.com>
2025-12-28 20:13:31 +0100

    feat: redesign search input and keyboard shortcuts styling
    - Replace EuiIcon with custom SVG search icon
    - Simplify ⌘K shortcut display (remove border/background, match placeholder style)
    - Update keyboard shortcuts pills styling:
      - Background: backgroundBaseHighlighted
      - Border: 1px solid borderBasePlain
      - Text color: textSubdued
      - Font-size: 11px, line-height: 16px
      - Different padding for icon vs text shortcuts
    - Adjust input padding to prevent text overlap with shortcut
    - Remove search-or-ask-ai from header

--- src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/NavigationSearch/SearchInput.tsx
+++ src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/NavigationSearch/SearchInput.tsx
@@ -1,5 +1,28 @@
-import { EuiIcon, EuiLoadingSpinner, useEuiTheme } from '@elastic/eui'
+import { EuiLoadingSpinner, useEuiTheme } from '@elastic/eui'
 import { css } from '@emotion/react'
+
+const CustomSearchIcon = () => {
+    const { euiTheme } = useEuiTheme()
+    
+    return (
+        <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            css={css`
+                color: ${euiTheme.colors.textDisabled};
+                flex-shrink: 0;
+            `}
+        >
+            <path
+                d="M7.87891 8.87891C9.05048 7.70735 10.9495 7.70735 12.1211 8.87891C13.172 9.93001 13.2802 11.5668 12.4453 12.7383L13.8535 14.1465L13.1465 14.8535L11.7383 13.4453C10.5668 14.2802 8.93001 14.172 7.87891 13.1211C6.70735 11.9495 6.70734 10.0505 7.87891 8.87891ZM5 13H2V12H5V13ZM11.4141 9.58594C10.633 8.80491 9.36698 8.80491 8.58594 9.58594C7.8049 10.367 7.8049 11.633 8.58594 12.4141C9.367 13.1949 10.6331 13.195 11.4141 12.4141C12.195 11.6331 12.1949 10.367 11.4141 9.58594ZM6 10H2V9H6V10ZM14 7H2V6H14V7ZM14 4H2V3H14V4Z"
+                fill="currentColor"
+            />
+        </svg>
+    )
+}
 
 export interface SearchInputProps {
     inputRef: React.RefObject<HTMLInputElement>
@@ -44,7 +67,7 @@ export const SearchInput = ({
                 {isLoading ? (
                     <EuiLoadingSpinner size="m" />
                 ) : (
-                    <EuiIcon type="search" size="m" color="subdued" />
+                    <CustomSearchIcon />
                 )}
             </span>
 
@@ -62,7 +85,7 @@ export const SearchInput = ({
                     width: 100%;
                     padding: ${euiTheme.size.s} ${euiTheme.size.m};
                     padding-left: 34px;
-                    padding-right: 62px;
+                    padding-right: calc(${euiTheme.size.m} + 2ch + ${euiTheme.size.m});
                     border: 1px solid ${euiTheme.colors.borderBasePlain};
                     border-radius: ${euiTheme.border.radius.medium};
                     background: ${euiTheme.colors.backgroundBasePlain};
@@ -84,20 +107,13 @@ export const SearchInput = ({
             <span
                 css={css`
                     position: absolute;
-                    right: ${euiTheme.size.s};
-                    display: flex;
+                    right: ${euiTheme.size.m};
+                    display: inline-flex;
                     align-items: center;
                     pointer-events: none;
-                    height: ${euiTheme.size.l};
-                    padding: 0 ${euiTheme.size.m};
-                    gap: ${euiTheme.size.s};
-                    border-radius: ${euiTheme.border.radius.small};
-                    border: 1px solid ${euiTheme.colors.borderBasePlain};
-                    background: ${euiTheme.colors.backgroundBasePlain};
                     color: ${euiTheme.colors.textDisabled};
-                    font-size: ${euiTheme.size.m};
-                    font-weight: ${euiTheme.font.weight.regular};
-                    line-height: ${euiTheme.size.base};
+                    font-size: ${euiTheme.font.scale.s * euiTheme.base}px;
+                    line-height: ${euiTheme.base * 1.25}px;
                 `}
             >
                 ⌘K
