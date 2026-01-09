722bae8d7721e574472776dc09b8e1dff9f87cd3
Martijn Laarman <Mpdreamz@gmail.com>
2026-01-09 17:38:33 +0100

    staging

--- src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/NavigationSearch/index.ts
+++
@@ -1,1 +0,0 @@
-export { NavigationSearch } from './NavigationSearch'
--- src/Elastic.Documentation.Site/Assets/web-components/SearchOrAskAi/NavigationSearch/useNavigationSearchRateLimitHandler.ts
+++
@@ -1,8 +0,0 @@
-import { ApiError } from '../errorHandling'
-import { useRateLimitHandler } from '../useRateLimitHandler'
-
-export function useNavigationSearchRateLimitHandler(
-    error: ApiError | Error | null
-) {
-    useRateLimitHandler('search', error)
-}
