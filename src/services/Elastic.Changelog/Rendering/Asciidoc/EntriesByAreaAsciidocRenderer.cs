e794b2192b819ac54098e63898d39434063e47f1
Felipe Cotti <felipe.cotti@elastic.co>
2026-01-21 14:41:03 -0300

    Adjust using directives after conflicts

--- src/services/Elastic.Changelog/Rendering/Asciidoc/EntriesByAreaAsciidocRenderer.cs
+++ src/services/Elastic.Changelog/Rendering/Asciidoc/EntriesByAreaAsciidocRenderer.cs
@@ -4,6 +4,7 @@
 
 using System.Globalization;
 using System.Text;
+using Elastic.Documentation.Changelog;
 
 namespace Elastic.Changelog.Rendering.Asciidoc;
 
--- src/services/Elastic.Changelog/Rendering/Markdown/IndexMarkdownRenderer.cs
+++ src/services/Elastic.Changelog/Rendering/Markdown/IndexMarkdownRenderer.cs
@@ -4,6 +4,7 @@
 
 using System.IO.Abstractions;
 using System.Text;
+using Elastic.Documentation.Changelog;
 using static System.Globalization.CultureInfo;
 
 namespace Elastic.Changelog.Rendering.Markdown;
