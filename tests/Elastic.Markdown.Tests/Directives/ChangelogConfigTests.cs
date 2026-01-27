57d5aa96af80eabaf20d255e35ac45f1d7c148b7
Felipe Cotti <felipe.cotti@elastic.co>
2026-01-27 13:35:12 -0300

    Split changelog tests for readability

---
+++ tests/Elastic.Markdown.Tests/Directives/ChangelogConfigTests.cs
@@ -0,0 +1,451 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.IO.Abstractions.TestingHelpers;
+using Elastic.Markdown.Myst.Directives.Changelog;
+using FluentAssertions;
+
+namespace Elastic.Markdown.Tests.Directives;
+
+public class ChangelogConfigLoadAutoDiscoverTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigLoadAutoDiscoverTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		// Create bundles with entries of different types
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+- title: Deprecation notice
+  type: deprecation
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  description: This API is deprecated.
+  impact: Users should migrate.
+  action: Use the new API.
+  pr: "222222"
+- title: Known issue
+  type: known-issue
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  description: There is a known issue.
+  impact: Some users may be affected.
+  pr: "333333"
+"""));
+
+		// Add changelog config with publish blockers
+		FileSystem.AddFile("docs/changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    types:
+      - deprecation
+      - known-issue
+"""));
+	}
+
+	[Fact]
+	public void LoadsPublishBlockerFromConfig() => Block!.PublishBlocker.Should().NotBeNull();
+
+	[Fact]
+	public void PublishBlockerHasCorrectTypes()
+	{
+		Block!.PublishBlocker!.Types.Should().NotBeNull();
+		Block!.PublishBlocker!.Types.Should().Contain("deprecation");
+		Block!.PublishBlocker!.Types.Should().Contain("known-issue");
+	}
+
+	[Fact]
+	public void FilteredEntriesExcludeBlockedTypes()
+	{
+		// Deprecation and known-issue entries should be filtered out
+		Html.Should().Contain("Regular feature");
+		Html.Should().NotContain("Deprecation notice");
+		Html.Should().NotContain("Known issue");
+	}
+
+	[Fact]
+	public void RendersFeaturesSection() => Html.Should().Contain("Features and enhancements");
+
+	[Fact]
+	public void DoesNotRenderDeprecationsSection() => Html.Should().NotContain("Deprecations");
+
+	[Fact]
+	public void DoesNotRenderKnownIssuesSection() => Html.Should().NotContain("Known issues");
+}
+
+public class ChangelogConfigLoadExplicitPathTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigLoadExplicitPathTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:config: custom/path/my-changelog.yml
+:::
+""")
+	{
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+- title: Internal docs
+  type: docs
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  areas:
+  - Internal
+  pr: "222222"
+"""));
+
+		// Add custom config at explicit path
+		FileSystem.AddFile("docs/custom/path/my-changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    areas:
+      - Internal
+"""));
+	}
+
+	[Fact]
+	public void ConfigPathPropertyIsSet() => Block!.ConfigPath.Should().Be("custom/path/my-changelog.yml");
+
+	[Fact]
+	public void LoadsPublishBlockerFromExplicitConfig() => Block!.PublishBlocker.Should().NotBeNull();
+
+	[Fact]
+	public void PublishBlockerHasCorrectAreas()
+	{
+		Block!.PublishBlocker!.Areas.Should().NotBeNull();
+		Block!.PublishBlocker!.Areas.Should().Contain("Internal");
+	}
+
+	[Fact]
+	public void FilteredEntriesExcludeBlockedAreas()
+	{
+		Html.Should().Contain("Regular feature");
+		Html.Should().NotContain("Internal docs");
+	}
+}
+
+public class ChangelogConfigLoadFromDocsSubfolderTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigLoadFromDocsSubfolderTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+- title: Other change
+  type: other
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "222222"
+"""));
+
+		// Add config in docs/docs/changelog.yml (docs subfolder)
+		FileSystem.AddFile("docs/docs/changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    types:
+      - other
+"""));
+	}
+
+	[Fact]
+	public void LoadsPublishBlockerFromDocsSubfolder() => Block!.PublishBlocker.Should().NotBeNull();
+
+	[Fact]
+	public void FilteredEntriesExcludeBlockedTypes()
+	{
+		Html.Should().Contain("Regular feature");
+		Html.Should().NotContain("Other change");
+	}
+}
+
+public class ChangelogConfigNotFoundTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigNotFoundTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""") => FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+"""));
+
+	[Fact]
+	public void PublishBlockerIsNullWhenNoConfig() => Block!.PublishBlocker.Should().BeNull();
+
+	[Fact]
+	public void RendersAllEntriesWhenNoConfig() => Html.Should().Contain("Regular feature");
+
+	[Fact]
+	public void NoErrorsEmittedForMissingConfig() =>
+		Collector.Diagnostics.Should().NotContain(d => d.Message.Contains("changelog.yml"));
+}
+
+public class ChangelogConfigExplicitPathNotFoundTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigExplicitPathNotFoundTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:config: nonexistent/config.yml
+:::
+""") => FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+"""));
+
+	[Fact]
+	public void PublishBlockerIsNullWhenExplicitConfigNotFound() => Block!.PublishBlocker.Should().BeNull();
+
+	[Fact]
+	public void EmitsWarningForMissingExplicitConfig() =>
+		Collector.Diagnostics.Should().Contain(d => d.Message.Contains("nonexistent/config.yml") && d.Message.Contains("not found"));
+
+	[Fact]
+	public void RendersAllEntriesWhenConfigNotFound() => Html.Should().Contain("Regular feature");
+}
+
+public class ChangelogConfigPriorityTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigPriorityTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+- title: Deprecation notice
+  type: deprecation
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  description: Deprecated.
+  impact: None.
+  action: Upgrade.
+  pr: "222222"
+- title: Other change
+  type: other
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "333333"
+"""));
+
+		// Add both config files - root should take priority
+		FileSystem.AddFile("docs/changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    types:
+      - deprecation
+"""));
+
+		FileSystem.AddFile("docs/docs/changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    types:
+      - other
+"""));
+	}
+
+	[Fact]
+	public void RootConfigTakesPriorityOverDocsSubfolder()
+	{
+		// Root config blocks deprecation, not other
+		Html.Should().Contain("Regular feature");
+		Html.Should().NotContain("Deprecation notice");
+		Html.Should().Contain("Other change");
+	}
+}
+
+public class ChangelogConfigEmptyBlockTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigEmptyBlockTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+"""));
+
+		// Config file exists but has no block section
+		FileSystem.AddFile("docs/changelog.yml", new MockFileData(
+"""
+lifecycles:
+  - preview
+  - beta
+  - ga
+"""));
+	}
+
+	[Fact]
+	public void PublishBlockerIsNullWhenNoBlockSection() => Block!.PublishBlocker.Should().BeNull();
+
+	[Fact]
+	public void RendersAllEntriesWhenNoBlockSection() => Html.Should().Contain("Regular feature");
+}
+
+public class ChangelogConfigMixedBlockersTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogConfigMixedBlockersTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Regular feature in Search
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  areas:
+  - Search
+  pr: "111111"
+- title: Deprecation in Search
+  type: deprecation
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  areas:
+  - Search
+  description: Deprecated.
+  impact: None.
+  action: Upgrade.
+  pr: "222222"
+- title: Feature in Internal
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  areas:
+  - Internal
+  pr: "333333"
+- title: Bug fix
+  type: bug-fix
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "444444"
+"""));
+
+		// Config with both type and area blockers
+		FileSystem.AddFile("docs/changelog.yml", new MockFileData(
+"""
+block:
+  publish:
+    types:
+      - deprecation
+    areas:
+      - Internal
+"""));
+	}
+
+	[Fact]
+	public void PublishBlockerHasBothTypesAndAreas()
+	{
+		Block!.PublishBlocker.Should().NotBeNull();
+		Block!.PublishBlocker!.Types.Should().Contain("deprecation");
+		Block!.PublishBlocker!.Areas.Should().Contain("Internal");
+	}
+
+	[Fact]
+	public void FiltersEntriesMatchingBlockedTypes() => Html.Should().NotContain("Deprecation in Search");
+
+	[Fact]
+	public void FiltersEntriesMatchingBlockedAreas() => Html.Should().NotContain("Feature in Internal");
+
+	[Fact]
+	public void RendersNonBlockedEntries()
+	{
+		Html.Should().Contain("Regular feature in Search");
+		Html.Should().Contain("Bug fix");
+	}
+}
---
+++ tests/Elastic.Markdown.Tests/Directives/ChangelogVersionSortingTests.cs
@@ -0,0 +1,263 @@
+// Licensed to Elasticsearch B.V under one or more agreements.
+// Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information
+
+using System.IO.Abstractions.TestingHelpers;
+using Elastic.Markdown.Myst.Directives.Changelog;
+using FluentAssertions;
+
+namespace Elastic.Markdown.Tests.Directives;
+
+public class ChangelogDateVersionedBundlesTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogDateVersionedBundlesTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		// Create multiple bundles with date-based versions (Cloud Serverless style)
+		FileSystem.AddFile("docs/changelog/bundles/2025-08-01.yaml", new MockFileData(
+"""
+products:
+- product: cloud-serverless
+  target: 2025-08-01
+entries:
+- title: August 1st feature
+  type: feature
+  products:
+  - product: cloud-serverless
+    target: 2025-08-01
+  pr: "111111"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/2025-08-15.yaml", new MockFileData(
+"""
+products:
+- product: cloud-serverless
+  target: 2025-08-15
+entries:
+- title: August 15th feature
+  type: feature
+  products:
+  - product: cloud-serverless
+    target: 2025-08-15
+  pr: "222222"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/2025-08-05.yaml", new MockFileData(
+"""
+products:
+- product: cloud-serverless
+  target: 2025-08-05
+entries:
+- title: August 5th feature
+  type: feature
+  products:
+  - product: cloud-serverless
+    target: 2025-08-05
+  pr: "333333"
+"""));
+	}
+
+	[Fact]
+	public void LoadsBundles() => Block!.LoadedBundles.Should().HaveCount(3);
+
+	[Fact]
+	public void RendersInDateOrderDescending()
+	{
+		// Should be sorted by date descending: 2025-08-15 > 2025-08-05 > 2025-08-01
+		var idx15 = Html.IndexOf("2025-08-15", StringComparison.Ordinal);
+		var idx05 = Html.IndexOf("2025-08-05", StringComparison.Ordinal);
+		var idx01 = Html.IndexOf("2025-08-01", StringComparison.Ordinal);
+
+		idx15.Should().BeLessThan(idx05, "2025-08-15 should appear before 2025-08-05");
+		idx05.Should().BeLessThan(idx01, "2025-08-05 should appear before 2025-08-01");
+	}
+
+	[Fact]
+	public void RendersAllDateVersions()
+	{
+		Html.Should().Contain("2025-08-15");
+		Html.Should().Contain("2025-08-05");
+		Html.Should().Contain("2025-08-01");
+	}
+
+	[Fact]
+	public void RendersEntriesForDateVersions()
+	{
+		Html.Should().Contain("August 15th feature");
+		Html.Should().Contain("August 5th feature");
+		Html.Should().Contain("August 1st feature");
+	}
+}
+
+public class ChangelogMixedVersionTypesTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogMixedVersionTypesTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		// Create bundles with mixed version types (semver and dates)
+		FileSystem.AddFile("docs/changelog/bundles/9.3.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.3.0
+entries:
+- title: Semver 9.3.0 feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.3.0
+  pr: "111111"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/9.2.0.yaml", new MockFileData(
+"""
+products:
+- product: elasticsearch
+  target: 9.2.0
+entries:
+- title: Semver 9.2.0 feature
+  type: feature
+  products:
+  - product: elasticsearch
+    target: 9.2.0
+  pr: "222222"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/2025-08-05.yaml", new MockFileData(
+"""
+products:
+- product: cloud-serverless
+  target: 2025-08-05
+entries:
+- title: Date-based feature
+  type: feature
+  products:
+  - product: cloud-serverless
+    target: 2025-08-05
+  pr: "333333"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/2025-07-01.yaml", new MockFileData(
+"""
+products:
+- product: cloud-serverless
+  target: 2025-07-01
+entries:
+- title: Earlier date feature
+  type: feature
+  products:
+  - product: cloud-serverless
+    target: 2025-07-01
+  pr: "444444"
+"""));
+	}
+
+	[Fact]
+	public void LoadsAllBundles() => Block!.LoadedBundles.Should().HaveCount(4);
+
+	[Fact]
+	public void SemverVersionsAppearBeforeDates()
+	{
+		// Semver versions should appear before date versions
+		var idx93 = Html.IndexOf("9.3.0", StringComparison.Ordinal);
+		var idx92 = Html.IndexOf("9.2.0", StringComparison.Ordinal);
+		var idxDate1 = Html.IndexOf("2025-08-05", StringComparison.Ordinal);
+		var idxDate2 = Html.IndexOf("2025-07-01", StringComparison.Ordinal);
+
+		// Semver versions should come before dates
+		idx93.Should().BeLessThan(idxDate1, "Semver 9.3.0 should appear before date 2025-08-05");
+		idx92.Should().BeLessThan(idxDate1, "Semver 9.2.0 should appear before date 2025-08-05");
+
+		// Within semver, should be sorted correctly
+		idx93.Should().BeLessThan(idx92, "9.3.0 should appear before 9.2.0");
+
+		// Within dates, should be sorted correctly (descending)
+		idxDate1.Should().BeLessThan(idxDate2, "2025-08-05 should appear before 2025-07-01");
+	}
+
+	[Fact]
+	public void RendersAllVersions()
+	{
+		Html.Should().Contain("9.3.0");
+		Html.Should().Contain("9.2.0");
+		Html.Should().Contain("2025-08-05");
+		Html.Should().Contain("2025-07-01");
+	}
+
+	[Fact]
+	public void RendersAllEntries()
+	{
+		Html.Should().Contain("Semver 9.3.0 feature");
+		Html.Should().Contain("Semver 9.2.0 feature");
+		Html.Should().Contain("Date-based feature");
+		Html.Should().Contain("Earlier date feature");
+	}
+}
+
+public class ChangelogRawVersionFallbackTests : DirectiveTest<ChangelogBlock>
+{
+	public ChangelogRawVersionFallbackTests(ITestOutputHelper output) : base(output,
+"""
+:::{changelog}
+:::
+""")
+	{
+		// Create bundles with non-standard version formats (edge case)
+		FileSystem.AddFile("docs/changelog/bundles/release-alpha.yaml", new MockFileData(
+"""
+products:
+- product: experimental
+  target: release-alpha
+entries:
+- title: Alpha release feature
+  type: feature
+  products:
+  - product: experimental
+    target: release-alpha
+  pr: "111111"
+"""));
+
+		FileSystem.AddFile("docs/changelog/bundles/release-beta.yaml", new MockFileData(
+"""
+products:
+- product: experimental
+  target: release-beta
+entries:
+- title: Beta release feature
+  type: feature
+  products:
+  - product: experimental
+    target: release-beta
+  pr: "222222"
+"""));
+	}
+
+	[Fact]
+	public void LoadsBundles() => Block!.LoadedBundles.Should().HaveCount(2);
+
+	[Fact]
+	public void RendersNonStandardVersions()
+	{
+		// Both non-standard versions should be rendered (sorted lexicographically)
+		Html.Should().Contain("release-alpha");
+		Html.Should().Contain("release-beta");
+		Html.Should().Contain("Alpha release feature");
+		Html.Should().Contain("Beta release feature");
+	}
+
+	[Fact]
+	public void SortsLexicographically()
+	{
+		// "release-beta" > "release-alpha" lexicographically
+		var idxBeta = Html.IndexOf("release-beta", StringComparison.Ordinal);
+		var idxAlpha = Html.IndexOf("release-alpha", StringComparison.Ordinal);
+
+		idxBeta.Should().BeLessThan(idxAlpha, "release-beta should appear before release-alpha (descending lexicographic)");
+	}
+}
