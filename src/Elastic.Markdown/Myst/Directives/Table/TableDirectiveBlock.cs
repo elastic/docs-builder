c1a702f5538103b5249d46438e2743c0bf880b87
Jan Calanog <jan.calanog@elastic.co>
2026-03-04 22:27:00 +0100

    Fix import order

--- src/Elastic.Markdown/Myst/Directives/Table/TableDirectiveBlock.cs
+++ src/Elastic.Markdown/Myst/Directives/Table/TableDirectiveBlock.cs
@@ -2,10 +2,10 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using System.Linq;
 using Elastic.Markdown.Diagnostics;
 using Markdig.Extensions.Tables;
 using Markdig.Syntax;
-using System.Linq;
 
 namespace Elastic.Markdown.Myst.Directives.Table;
 
