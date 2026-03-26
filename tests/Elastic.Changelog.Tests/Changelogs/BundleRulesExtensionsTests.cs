9f2cf5fd223c674ffdab34c9e727df0ec85ca031
lcawl <lcawley@elastic.co>
2026-03-26 16:55:10 -0700

    Fix usage of AwesomeAssertions

--- tests/Elastic.Changelog.Tests/Changelogs/BundleRulesExtensionsTests.cs
+++ tests/Elastic.Changelog.Tests/Changelogs/BundleRulesExtensionsTests.cs
@@ -2,8 +2,8 @@
 // Elasticsearch B.V licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information
 
+using AwesomeAssertions;
 using Elastic.Documentation.Configuration.Changelog;
-using FluentAssertions;
 
 namespace Elastic.Changelog.Tests.Changelogs;
 
